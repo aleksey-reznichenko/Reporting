9c15728124061bbc9f54c1784f1b6d860d9f0aa4
Alex <lehareznichenko3@gmail.com>
2022-03-09 19:32:22 +0200

    update path to image

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,21 +11,21 @@ let tasks = tasksArray
 document.querySelector('.header__mode').addEventListener("click", e => {
     transition()
     if(document.documentElement.getAttribute('data-theme') === 'dark'){
-        e.currentTarget.querySelector('img').src = '../img/dark-mode.svg';
+        e.currentTarget.querySelector('img').src = 'img/dark-mode.svg';
         document.documentElement.setAttribute('data-theme', 'light');
         document.querySelector('.header__logo').children[0].src = 'img/mainLogoLight.svg';
         document.querySelectorAll('.select__arrow').forEach(item=> item.src = 'img/arrowDark.svg');
         document.querySelectorAll('.select__checkbox--arrow').forEach(item=> item.src = 'img/arrowDark.svg');
-        document.querySelector('.info__content--label').style.backgroundImage = 'url("../img/labelBlue.svg")';
+        document.querySelector('.info__content--label').style.backgroundImage = 'url("img/labelBlue.svg")';
         document.querySelectorAll('.table__column-name').forEach(item => item.children[1].src = 'img/sortDark.svg')
     }
     else {
-        e.currentTarget.querySelector('img').src = '../img/light-mode.svg';
+        e.currentTarget.querySelector('img').src = 'img/light-mode.svg';
         document.documentElement.setAttribute('data-theme', 'dark');
         document.querySelector('.header__logo').children[0].src = 'img/mainLogo.svg';
         document.querySelectorAll('.select__arrow').forEach(item=> item.src = 'img/arrow.svg');
         document.querySelectorAll('.select__checkbox--arrow').forEach(item => item.src = 'img/arrow.svg');
-        document.querySelector('.info__content--label').style.backgroundImage = "url('../img/label.svg')"
+        document.querySelector('.info__content--label').style.backgroundImage = "url('img/label.svg')"
         document.querySelectorAll('.table__column-name').forEach(item => item.children[1].src = 'img/sort.svg')
     }
     paginationF()
